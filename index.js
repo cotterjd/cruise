e32465afb19d88023af8eef1dbec3362653e91cc
Jordan Cotter <jrdn.cttr@gmail.com>
2018-07-11 21:35:33 -0500

    spilt more stuff out and styled mobile view

--- index.js
+++ index.js
@@ -1,97 +1,48 @@
+const countDown = document.createElement('div')
+countDown.className="days-til-cruise"
+countDown.innerHTML="hey"
+document.querySelector('.count-down').appendChild(countDown)
 
-// app.js
+const
+	cruiseDate = new Date("11/30/2019")
+, diff = (cruiseDate - new Date())/1000/60/60/24
+, daysTillCruise = Math.floor(diff)
+, daysTilCruiseElement = document.querySelector('.days-til-cruise')
+;
+daysTilCruiseElement.outerHTML=`${daysTillCruise} days until cruise!`
 
-window.addEventListener('load', function() {
+function addHTML(day) {
+	const
+		div = document.createElement('div')
+	;
+	div.className="day"
+	div.innerHTML = `
+		<p><strong>${day}</strong></p>
+		<div class="item">
+			<span>Morning</span>
+			<input placeholder="name" />
+			<input placeholder="number" />
+		</div>
+		<div class="item">
+			<span>Afternoon</span>
+			<input placeholder="name" />
+			<input placeholder="number" />
+		</div>
+		<div class="item">
+			<span>Evening</span>
+			<input placeholder="name" />
+			<input placeholder="number" />
+		</div>
+		<div class="item">
+			<span>Night</span>
+			<input placeholder="name" />
+			<input placeholder="number" />
+		</div>
+	`
+	document.querySelector('.container').appendChild(div)
+}
 
-  var webAuth = new auth0.WebAuth({
-    domain: 'parent-vacation.auth0.com',
-    clientID: 'P2wT0BrYqrjQsMxh2Thyn5A1nSX96drd',
-    responseType: 'token id_token',
-    audience: 'https://parent-vacation.auth0.com/userinfo',
-    scope: 'openid',
-    redirectUri: window.location.href
-  });
-
-  var loginBtn = document.getElementById('btn-login');
-
-  loginBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    webAuth.authorize();
-  });
-  //var loginStatus = document.querySelector('.container h4');
-  //var loginView = document.getElementById('login-view');
-  //var homeView = document.getElementById('home-view');
-
-  // buttons and event listeners
-  //var homeViewBtn = document.getElementById('btn-home-view');
-  var loginBtn = document.getElementById('btn-login');
-  var logoutBtn = document.getElementById('btn-logout');
-
- // homeViewBtn.addEventListener('click', function() {
- // 		// show values
- //   homeView.style.display = 'inline-block';
- //   loginView.style.display = 'none';
- // });
-
-  logoutBtn.addEventListener('click', logout);
-
-  function handleAuthentication() {
-    webAuth.parseHash(function(err, authResult) {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
-        setSession(authResult);
-        loginBtn.style.display = 'none';
- //       homeView.style.display = 'inline-block';
-      } else if (err) {
-  //      homeView.style.display = 'inline-block';
-        console.log(err);
-        alert(
-          'Error: ' + err.error + '. Check the console for further details.'
-        );
-      }
-      displayButtons();
-    });
-  }
-
-  function setSession(authResult) {
-    // Set the time that the Access Token will expire at
-    var expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
-  }
-
-  function logout() {
-    // Remove tokens and expiry time from localStorage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-    displayButtons();
-  }
-
-  function isAuthenticated() {
-    // Check whether the current time is past the
-    // Access Token's expiry time
-    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
-  }
-
-  function displayButtons() {
-    if (isAuthenticated()) {
-			// show values
-      loginBtn.style.display = 'none';
-      logoutBtn.style.display = 'inline-block';
- //     loginStatus.innerHTML = 'You are logged in!';
-    } else {
-			// hide values
-      loginBtn.style.display = 'inline-block';
-      logoutBtn.style.display = 'none';
- //     loginStatus.innerHTML =
- //       'You are not logged in! Please log in to continue.';
-    }
-  }
-
-	handleAuthentication();
+const arr = [ "Nov. 30", "Dec. 1st", "Dec. 2nd", "Dec. 3rd", "Dec. 4th", "Dec. 5th" ]
+arr.forEach(x => {
+		addHTML(x)
 });
